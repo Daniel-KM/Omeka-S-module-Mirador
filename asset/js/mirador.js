a1c795795edd2617479870ec42689a05d5f991d3
Daniel Berthereau <Daniel.git@Berthereau.net>
2021-05-03 00:00:00 +0000

    Fixed use without plugin.

--- asset/js/mirador.js
+++ asset/js/mirador.js
@@ -1,8 +1,10 @@
 jQuery(document).ready(async function(){
     let plugins = [];
-    for (let {plugin, name} of window.miradorPlugins) {
-        if (window.globalMiradorPlugins.includes(name)) {
-            plugins = [...plugins, ...plugin];
+    if (window.miradorPlugins && window.miradorPlugins.length) {
+        for (let {plugin, name} of window.miradorPlugins) {
+            if (window.globalMiradorPlugins.includes(name)) {
+                plugins = [...plugins, ...plugin];
+            }
         }
     }
     await Mirador.viewer(miradorConfig, plugins);
