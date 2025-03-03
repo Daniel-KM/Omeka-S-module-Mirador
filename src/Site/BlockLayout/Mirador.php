956a5e7ae7a54bcd970d0f912b3efd099831e8c5
Daniel Berthereau <Daniel.git@Berthereau.net>
2025-03-03 00:00:00 +0000

    Added support of templatable block.

--- src/Site/BlockLayout/Mirador.php
+++ src/Site/BlockLayout/Mirador.php
@@ -1,32 +1,5 @@
 <?php declare(strict_types=1);
 
-/*
- * Copyright 2015-2024 Daniel Berthereau
- *
- * This software is governed by the CeCILL license under French law and abiding
- * by the rules of distribution of free software. You can use, modify and/or
- * redistribute the software under the terms of the CeCILL license as circulated
- * by CEA, CNRS and INRIA at the following URL "http://www.cecill.info".
- *
- * As a counterpart to the access to the source code and rights to copy, modify
- * and redistribute granted by the license, users are provided only with a
- * limited warranty and the software’s author, the holder of the economic
- * rights, and the successive licensors have only limited liability.
- *
- * In this respect, the user’s attention is drawn to the risks associated with
- * loading, using, modifying and/or developing or reproducing the software by
- * the user in light of its specific status of free software, that may mean that
- * it is complicated to manipulate, and that also therefore means that it is
- * reserved for developers and experienced professionals having in-depth
- * computer knowledge. Users are therefore encouraged to load and test the
- * software’s suitability as regards their requirements in conditions enabling
- * the security of their systems and/or data to be ensured and, more generally,
- * to use and operate it in the same conditions as regards security.
- *
- * The fact that you are presently reading this means that you have had
- * knowledge of the CeCILL license and that you accept its terms.
- */
-
 namespace Mirador\Site\BlockLayout;
 
 use Laminas\View\Renderer\PhpRenderer;
@@ -34,9 +7,15 @@ use Omeka\Api\Representation\SitePageBlockRepresentation;
 use Omeka\Api\Representation\SitePageRepresentation;
 use Omeka\Api\Representation\SiteRepresentation;
 use Omeka\Site\BlockLayout\AbstractBlockLayout;
+use Omeka\Site\BlockLayout\TemplateableBlockLayoutInterface;
 
-class Mirador extends AbstractBlockLayout
+class Mirador extends AbstractBlockLayout implements TemplateableBlockLayoutInterface
 {
+    /**
+     * The default partial view script.
+     */
+    const PARTIAL_NAME = 'common/block-layout/mirador';
+
     public function getLabel()
     {
         return 'Mirador Viewer'; // @translate
@@ -51,14 +30,14 @@ class Mirador extends AbstractBlockLayout
         return $view->blockAttachmentsForm($block);
     }
 
-    public function render(PhpRenderer $view, SitePageBlockRepresentation $block)
+    public function render(PhpRenderer $view, SitePageBlockRepresentation $block, $templateViewScript = self::PARTIAL_NAME)
     {
         $attachments = $block->attachments();
         if (!$attachments) {
             return 'No resource selected'; // @translate
         }
 
-        return $view->partial('common/block-layout/mirador', [
+        return $view->partial($templateViewScript, [
             'block' => $block,
             'attachments' => $attachments,
         ]);
